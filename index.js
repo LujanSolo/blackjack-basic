9674f4606b854eb672c81c6c12618783ec7721c9
LujanSolo <lujansolo@proton.me>
2023-08-26 12:05:09 -0700

    added a scoreboard function

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
+//* GAME START CONFIRMATION (ENTRY POINT)
 let gameStart = confirm("START GAME?");
 
+//* OBJECT TO HOLD GAME DETAILS
 let gameDetails = {
   playerScore: 0,
   dealerScore: 0,
   scoreLimit: 21,
-}
+};
 
+//* CONDITIONAL FOR GAME START
 if (gameStart) {
   blackjack();
 } else {
   alert("Maybe another time...");
 }
 
+//* FUNCTION TO DRAW A SINGLE CARD
 function drawOne() {
   return Math.floor(Math.random() * (11 - 2 + 1) + 2);
 }
 
-function blackjack() {
-  // let player add one random card to his total every time he confirms "draw one"
-  gameDetails.playerScore += drawOne() + drawOne();
-  gameDetails.dealerScore += drawOne();
-
-  
-
-  alert(`
-    You are given two cards. The dealer reveals one of his two.
+function scoreboard() {
+  alert(
+    `
     Your Score: ${gameDetails.playerScore}
     Dealer's Score: ${gameDetails.dealerScore}
     `
   );
+}
 
-  if(gameDetails.playerScore === 21){
+//* BASIC GAME FUNCTIONALITY
+function blackjack() {
+  gameDetails.playerScore += drawOne() + drawOne(); // sets opening player score
+  gameDetails.dealerScore += drawOne();  // sets opening dealer score
+
+  // Since Aces can be valued at 1 or 11, starting values will never be double 11's
+  // hence, we reset values of 22 to 21 for the player
+  if (gameDetails.playerScore === 22) {
+    gameDetails.playerScore = 21;
+  }
+
+  // SCOREBOARD (function?)
+  alert("You are given two cards. The dealer reveals one of his two cards.")
+  scoreboard();
+
+  if (gameDetails.playerScore === 21) {
     alert("BLACKJACK! YOU WIN!");
     endGame();
-  };
-};
+  }
+}
 
-function endGame() {alert(`Your Score: ${gameDetails.playerScore} Dealer's Score: ${gameDetails.dealerScore} GAME OVER`); return;}
+function endGame() {
+  alert(
+    `
+    FINAL SCORE
+    YOUR SCORE: ${gameDetails.playerScore}
+    DEALER'S SCORE: ${gameDetails.dealerScore}
+    `
+  )
+  return;
+}
